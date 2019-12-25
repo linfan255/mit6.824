da53ff1d9528fe907b58dec37c26247c93614f47
linfan255 <linfan255@163.com>
2019-12-25 10:56:32 +0800

    passed Lab2B, but running time spent more than 1 min

--- src/raft/test_test.go
+++ src/raft/test_test.go
@@ -38,7 +38,7 @@ func TestInitialElection2A(t *testing.T) {
 	time.Sleep(2 * RaftElectionTimeout)
 	term2 := cfg.checkTerms()
 	if term1 != term2 {
-		fmt.Printf("warning: term changed even though there were no failures. term1(%d) vs term2(%d)", term1, term2)
+		fmt.Printf("warning: term changed even though there were no failures")
 	}
 
 	// there should still be a leader.
